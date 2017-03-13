6ecf67387c9d5f301b5b1339cb4e20e938a3135f
geekerhua <geekerhua@sina.com>
2017-03-13 09:45:42 +0800

    add js计时器

--- 25-js/5-数组.js
+++ 25-js/5-数组.js
@@ -6,13 +6,12 @@ console.log(aR1.join('-'));
 console.log(aR1.push('haha'));
 console.log(aR1);
 // pop
+console.log('------');
 console.log(aR1.pop());
 console.log(aR1);
 
 console.log('------');
-
 console.log(aR1.unshift(333));
-console.log('------');
 console.log(aR1.reverse());
 console.log(aR1);
 
@@ -21,12 +20,13 @@ console.log(aR1.indexOf(1));
 console.log(aR1.lastIndexOf(333));
 
 console.log('------');
+// 在数组中增加或者删除成员
 console.log(aR1.splice(2,2,444,6666));
 console.log(aR1);
 
 // 数组去重
-aLi = [1,2,4,5,1,4,7,2,4];
-aLi2 = [];
+var aLi = [1,2,4,5,1,4,7,2,4];
+var aLi2 = [];
 for (var i = 0; i < aLi.length; i++) {
     if (aLi.indexOf(aLi[i]) == i) {
         aLi2.push(aLi[i])
