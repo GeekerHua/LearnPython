1be0d0e6600877d05d0d9d00c8be8a390fb47f2e
geekerhua <geekerhua@sina.com>
2017-03-26 09:02:25 +0800

    add test2 moduels

---
+++ 29-DjangoDemo/test2/booktest/migrations/0003_areainfo.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('booktest', '0002_auto_20170325_1029'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='AreaInfo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('atitle', models.CharField(max_length=20)),
+                ('aParent', models.ForeignKey(to='booktest.AreaInfo')),
+            ],
+        ),
+    ]
---
+++ 29-DjangoDemo/test2/booktest/views.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+from django.shortcuts import render, redirect
+# Create your views here.
+from models import *
+from datetime import date
+from django.db.models import F, Q
+
+
+def index(request):
+    return render(request, 'booktest/index.html')
+
+
+def list(request):
+    # list = BookInfo.objects.all()
+    # list = BookInfo.books.all()
+    # list = BookInfo.books.filter(id__exact=1)
+    # list = BookInfo.books.filter(btitle__contains='传')
+    # list = BookInfo.books.filter(btitle__endswith='部')
+    # list = BookInfo.books.filter(btitle__isnull=False)
+    # list = BookInfo.books.filter(pk__in=[1, 3, 5])
+    # list = BookInfo.books.filter(pk__gte=3)
+    # list = BookInfo.books.exclude(pk__gte=3)
+    # list = BookInfo.books.filter(bpub_date__year=1986)
+    # list = BookInfo.books.filter(bpub_date__gt=date(1991, 1, 1))
+    # list = BookInfo.books.filter(bpub_date__gt=date(1981, 1, 1), bread__gt=20)
+    # list = BookInfo.books.filter(heroinfo__hcontent__contains='八')
+    # list = HeroInfo.objects.filter(hbook__btitle='天龙八部')
+    # list = BookInfo.books.filter(bread__gte=F('bcommet') * 0.5)
+    # list = BookInfo.books.filter(bread__gt=10).filter(bread__gt=10)
+    # list = BookInfo.books.filter(Q(bread__gt=30) | Q(bcommet__gt=50))
+    list = BookInfo.books.filter(~Q(bread__gt=30))
+    count = BookInfo.books.count()
+    print count
+    # list = HeroInfo.objects.filter(hgender=True)
+    context = {'list': list}
+    return render(request, 'booktest/list.html', context)
+
+
+def create(request):
+    book = BookInfo.books.create_book('流星蝴蝶剑', date(2017, 3, 25), 0, 0)
+    book.save()
+    return redirect('/list/')
+
+
+def area(request):
+    city = AreaInfo.objects.get(pk=440100)
+    context = {'city': city}
+    return render(request, 'booktest/area.html', context)
